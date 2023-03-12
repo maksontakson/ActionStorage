0b55d70f7d5b1adfa9810f7d0fcb6571892b4662
maksontakson <maksontukson@gmail.com>
2023-03-12 16:59:49 +0200

    DB Rider used

--- src/test/java/app/just/actionstorage/repository/ActionRepositoryIntegrationTest.java
+++ src/test/java/app/just/actionstorage/repository/ActionRepositoryIntegrationTest.java
@@ -2,11 +2,11 @@ package app.just.actionstorage.repository;
 
 import static app.just.actionstorage.common.TestConstants.Model.ActionEntityAttributes.ACTIVE_EMAIL_SOURCE_ENTITY;
 
-import app.just.actionstorage.common.TestConstants;
 import app.just.actionstorage.entity.ActionEntity;
-import app.just.actionstorage.entity.SourceEntity;
-import app.just.actionstorage.entity.UserEntity;
 import app.just.common.dto.ActionType;
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
 import java.time.Instant;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
@@ -15,11 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@DBRider
 public class ActionRepositoryIntegrationTest extends AbstractTest {
   @Autowired
   private ActionRepository actionRepository;
-  @Autowired
-  private SourceRepository sourceRepository;
 
   private final ActionEntity actionEntity = ActionEntity.builder()
       .id("1")
@@ -29,8 +28,9 @@ public class ActionRepositoryIntegrationTest extends AbstractTest {
       .source(ACTIVE_EMAIL_SOURCE_ENTITY)
       .build();
   @Test
+  @DataSet(value = "source.yml")
+  @DBUnit(caseSensitiveTableNames = true)
   public void whenFindAll_thenReturnUserEntityList() {
-    sourceRepository.save(ACTIVE_EMAIL_SOURCE_ENTITY);
     List<ActionEntity> all = actionRepository.saveAll(List.of(actionEntity));
     Assertions.assertEquals(1, all.size());
   }
